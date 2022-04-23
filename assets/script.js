13d5ff8ce8a6377f7c9a366e52ff6adcbb1a59e4
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-23 13:50:26 -0400

    closer

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,27 @@ function weatherSearch(){
     .then(function(response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(data => {
+      console.log(data);
+      displayInfo(data)
+    })
         //call display info
-     var searchText = document.getElementById("search-button")
-     searchText.addEventListener("click", data)
-      console.log(searchText)
-      //document.getElementById("card").innerText = searchText
+    //  var searchText = document.getElementById("search-button")
+    //  searchText.addEventListener("click", data)
+    //   console.log(searchText)
+    
+      
 
-    });
+    };
 // get the city form the search box and take the data from the api and put it on the page
 // put weather on the page
-}
+
 
 
 function displayInfo(){
     // displaying weather info after search
+    const temp = data.temp[0];
+    const tempP = document.getElementById("temp")
     
 }
 
