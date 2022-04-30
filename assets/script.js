6d7eb388e2e5d58df1cf5a85c49d57c6191fdf29
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-30 01:52:23 -0400

    i tried

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,51 @@ function weatherSearch(city){
     };
 // get the city form the search box and take the data from the api and put it on the page
 // put weather on the page
+function getInfo(){
+  const newName = document.getElementById("cityInput");
+  const cityName = document.getElementById("cityName");
+  cityName.innerHTML ="--"+newName.value+"--"
+
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=eb75c7c4e7622ead3f14c1ae67623f07")
+.then(response => response.json())
+.then(data =>{
+  for(i=0;i<5;i++){
+    document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1);
+  }
+  for(i=0;i<5;i++){
+    document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1);
+  }
+  for(i=0;i<5;i++){
+    document.getElementById("img" +(i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather.[0].icon+".png";
+  }
+})
+
+.catch(err => alert("something went wrong!"))
+}
+
+function defaultScreen(){
+  document.getElementById("cityInput").defaultValue ="London";
+  getInfo();
+}
+
+const d =new Date();
+const weekday =["Sunday","Monday","Teusday","Wednesday","Thusday","Friday","Saturday"];
+
+function checkDay(day){
+  if(day +d.getDay() > 6){
+    return day +d.getDay()-7;
+  }
+  else{
+    return day +d.getDay();
+  }
+}
+
+for(i=o;i<5;i++){
+  document.getElementById("day"+(i+1)).innerHTML = weekday[checkDay(i)];
+}
+
 function weatherForecast(){
-  fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=daily&appid=eb75c7c4e7622ead3f14c1ae67623f07")
+  fetch ("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=eb75c7c4e7622ead3f14c1ae67623f07")
   .then(function(response) {
     return response.json();
   })
@@ -22,9 +65,7 @@ function weatherForecast(){
 function displayDaily(data){
   console.log(data);
 
-  const weather = daily.weather.description;
-  const forecastH2 = document.getElementById("forecast").textContent = weather
-  console.log(weather);
+
 
 };
 
@@ -40,8 +81,7 @@ function displayInfo(data) {
     const windSpeedDiv = document.getElementById("windSpeed").textContent = wind
     // const uvi = data.current.uvi;
     // const uviDiv = document.getElementById("uvi").textContent = uvi
-    const lon = data.coord.lon;
-    const lat = data.coord.lat;
+   
     
 }
 
