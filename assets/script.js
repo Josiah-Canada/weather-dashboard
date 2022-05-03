952655f23c81b8bd1caa8650017d5e81c82c2c4e
Josiah-Canada <josiahcanada@yahoo.com>
2022-05-02 21:05:49 -0400

    recall

--- assets/script.js
+++ assets/script.js
@@ -15,43 +15,7 @@ function getInfo(){
   const cityName = document.getElementById("cityName");
   cityName.innerHTML ="--"+newName.value+"--"
 
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=eb75c7c4e7622ead3f14c1ae67623f07")
-.then(response => response.json())
-.then(data =>{
-  for(i=0;i<5;i++){
-    document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1);
-  }
-  for(i=0;i<5;i++){
-    document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1);
-  }
-  for(i=0;i<5;i++){
-    document.getElementById("img" +(i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather.[0].icon+".png";
-  }
-})
-
-.catch(err => alert("something went wrong!"))
-}
-
-function defaultScreen(){
-  document.getElementById("cityInput").defaultValue ="London";
-  getInfo();
-}
-
-const d =new Date();
-const weekday =["Sunday","Monday","Teusday","Wednesday","Thusday","Friday","Saturday"];
-
-function checkDay(day){
-  if(day +d.getDay() > 6){
-    return day +d.getDay()-7;
-  }
-  else{
-    return day +d.getDay();
-  }
-}
-
-for(i=o;i<5;i++){
-  document.getElementById("day"+(i+1)).innerHTML = weekday[checkDay(i)];
-}
+ 
 
 function weatherForecast(){
   fetch ("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=eb75c7c4e7622ead3f14c1ae67623f07")
